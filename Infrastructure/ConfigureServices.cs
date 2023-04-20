e4354c7af2d4cf3ea9fb2f71a9933f0876913c51
dmytro.kirieiev <dmytro.kirieiev@cleverbridge.com>
2023-04-20 15:40:01 +0200

    finish feature

--- Infrastructure/ConfigureServices.cs
+++ Infrastructure/ConfigureServices.cs
@@ -1,5 +1,7 @@
 ﻿using Application.Interfaces;
+using Application.Services;
 using Infrastructure.Persistence;
+using Infrastructure.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -11,12 +13,12 @@ public static class ConfigureServices
     public static void AddInfrastructureServices(this IServiceCollection services,
         IConfiguration configuration)
     {
+        services.AddScoped<IEmailService, EmailService>();
         var dbConfiguration = configuration
             .GetSection("DbConfiguration")
             .Get<DbConfiguration>()!;
 
         var connectionString = dbConfiguration.GetConnectionString();
-
         services
             .AddDbContext<IApplicationDbContext, ApplicationDbContext>(builder =>
             {
