fa6c747f242bfc3ab36e78c370468e7895d6f60e
dimanoit <dimanoit@gmail.com>
2023-04-30 20:04:00 +0200

    new test proj

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,40 +1,45 @@
-using System.Text.Json.Serialization;
-using Api;
 using Api.Middlewares;
 using Application;
 using Infrastructure;
 using Serilog;
-using ConfigureServices = Api.ConfigureServices;
 
-var builder = WebApplication.CreateBuilder(args);
-builder.Logging.ClearProviders();
+namespace Api;
 
-var host = builder.Host;
-host.UseSerilog();
-
-var services = builder.Services;
-
-services.AddInfrastructureServices(builder.Configuration);
-services.AddApplicationServices();
-services.AddApiServices(builder.Configuration, builder.Environment);
-
-var app = builder.Build();
-
-app.UseMiddleware<ExceptionMiddleware>();
-if (app.Environment.IsDevelopment())
+public static class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public static async Task Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Logging.ClearProviders();
+
+        var host = builder.Host;
+        host.UseSerilog();
+
+        var services = builder.Services;
+
+        services.AddInfrastructureServices(builder.Configuration);
+        services.AddApplicationServices();
+        services.AddApiServices(builder.Configuration, builder.Environment);
+
+        var app = builder.Build();
+
+        app.UseMiddleware<ExceptionMiddleware>();
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseRouting()
+            .UseCors(ConfigureServices.CorsPolicyName);
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        await app.RunAsync();
+    }
 }
-
-app.UseRouting()
-    .UseCors(ConfigureServices.CorsPolicyName);
-
-app.UseHttpsRedirection();
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
