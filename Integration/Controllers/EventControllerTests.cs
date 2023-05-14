9195d039bcefebf9dc1ebcc4b3aabd41f70effca
dimanoit <dimanoit@gmail.com>
2023-05-14 16:27:02 +0200

    fix tests

--- Integration/Controllers/EventControllerTests.cs
+++ Integration/Controllers/EventControllerTests.cs
@@ -63,7 +63,7 @@ public class EventControllerTests : IClassFixture<WebApplicationFactory<Program>
     {
         // Arrange
         var client = _factory.CreateClient();
-        var url = "api/Event?skip=0&take=100";
+        var url = "api/event?skip=0&take=100";
 
         // Act
         var result = await client.GetAuth(url, SharedTestData.TestEmail);
@@ -83,7 +83,7 @@ public class EventControllerTests : IClassFixture<WebApplicationFactory<Program>
     {
         // Arrange
         var client = _factory.CreateClient();
-        var response = await client.GetAuth("api/Event?skip=0&take=100", SharedTestData.TestEmail);
+        var response = await client.GetAuth("api/event?skip=0&take=100", SharedTestData.TestEmail);
         var jsonResult = await response.Content.ReadAsStringAsync();
         var eventId = JsonDocument.Parse(jsonResult).RootElement.GetProperty("data")
             .EnumerateArray().Last()
