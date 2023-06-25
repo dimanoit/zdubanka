ba00bc752d157b4effb40736a3f6eeae3b8fa510
dimanoit <dimanoit@gmail.com>
2023-06-25 09:19:02 +0200

    add unit tests

---
+++ tests/ApplicationUnitTests/Fakers/EventFaker.cs
@@ -0,0 +1,78 @@
+﻿using Bogus;
+using Domain.Entities;
+using Domain.Enums;
+using Domain.Models;
+using Microsoft.AspNetCore.Mvc.Diagnostics;
+
+namespace ApplicationUnitTests.Fakers;
+
+public static class EventFaker
+{
+    private static Address GenerateAddress(Faker faker)
+    {
+        return new Address
+        {
+            Street = faker.Address.StreetAddress(),
+            City = faker.Address.City(),
+            State = faker.Address.State(),
+            Country = faker.Address.Country()
+        };
+    }
+
+
+    public static EventParticipant[] GenerateFakeEventParticipants(
+        (string EventId, ParticipantStatus Status) eventData, int count)
+    {
+        var result = new EventParticipant[count];
+        for (var i = 0; i < result.Length; i++)
+        {
+            result[i] = GenerateFakeEventParticipant(eventData.EventId, eventData.Status);
+        }
+
+        return result;
+    }
+
+    private static EventParticipant GenerateFakeEventParticipant(
+        string eventId,
+        ParticipantStatus status)
+    {
+        var faker = new Faker<EventParticipant>()
+            .RuleFor(p => p.UserId, f => f.Random.Guid().ToString())
+            .RuleFor(p => p.EventId, f => eventId)
+            .RuleFor(p => p.Status, f => status);
+
+        return faker.Generate();
+    }
+
+    private static EventLimitation GenerateEventLimitation(Faker faker, int countOfPeople)
+    {
+        return new EventLimitation
+        {
+            CountOfPeople = countOfPeople,
+            Gender = faker.PickRandom<Gender[]>(faker.Random.EnumValues(exclude: Gender.Other)),
+            RelationshipStatus = faker.PickRandom<RelationshipStatus[]>(faker.Random.EnumValues<RelationshipStatus>()),
+            AgeLimit = new AgeLimit
+            {
+                Min = faker.Random.Int(18, 30),
+                Max = faker.Random.Int(31, 50)
+            }
+        };
+    }
+
+    public static Event GenerateFakeEvent(int countOfPeople)
+    {
+        var faker = new Faker<Event>()
+            .RuleFor(e => e.Id, f => f.Random.Guid().ToString())
+            .RuleFor(e => e.Location, GenerateAddress)
+            .RuleFor(e => e.Title, f => f.Commerce.ProductName())
+            .RuleFor(e => e.Description, f => f.Lorem.Sentence())
+            .RuleFor(e => e.StartDay, f => f.Date.Future())
+            .RuleFor(e => e.EndDay, (f, e) => f.Date.Between(e.StartDay, e.StartDay.AddDays(7)))
+            .RuleFor(e => e.EventLimitation, f => GenerateEventLimitation(f, countOfPeople))
+            .RuleFor(e => e.Latitude, f => f.Address.Latitude())
+            .RuleFor(e => e.Longitude, f => f.Address.Longitude())
+            .RuleFor(e => e.OrganizerId, f => f.Random.Guid().ToString());
+
+        return faker.Generate();
+    }
+}
---
+++ tests/ApplicationUnitTests/Fixtures/EventParticipantsFixture.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using ApplicationUnitTests.Fakers;
+using Domain.Entities;
+using Domain.Enums;
+
+namespace ApplicationUnitTests.Fixtures;
+
+public class EventParticipantsFixture : IEnumerable<object[]>
+{
+    public IEnumerator<object[]> GetEnumerator()
+    {
+        yield return new object[] { GetEvent(), EventStatus.Closed };
+        yield return new object[] { EventFaker.GenerateFakeEvent(3), EventStatus.Opened };
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    private Event GetEvent()
+    {
+        var eventEntity = EventFaker.GenerateFakeEvent(3);
+        eventEntity.EventParticipants = EventFaker.GenerateFakeEventParticipants((eventEntity.Id, ParticipantStatus.Accepted), 3);
+
+        return eventEntity;
+    }
+}
