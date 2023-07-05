024cd663c10a8ff8dc90bb42b49345090a0693af
dimanoit <dimanoit@gmail.com>
2023-07-05 08:46:33 +0200

    cover queries and behaviours

---
+++ tests/ApplicationUnitTests/Queries/EventParticipantQueryHandlerTests.cs
@@ -0,0 +1,31 @@
+﻿using Application.Queries;
+using ApplicationUnitTests.Fakers;
+using ApplicationUnitTests.Helpers;
+using Domain.Requests;
+using FluentAssertions;
+using Xunit;
+
+namespace ApplicationUnitTests.Queries;
+
+public class EventParticipantQueryHandlerTests
+{
+    [Fact]
+    public async Task Handle_WithValidRequest_ReturnsEventsParticipantsResponse()
+    {
+        // Arrange
+        var eventWithParticipants = EventFaker.CreateEventWithParticipants();
+        var request = new EventParticipantRequest(eventWithParticipants.Id, eventWithParticipants.OrganizerId);
+        var query = new EventParticipantQuery(request);
+        var dbContext = ApplicationDbContextFactory.Create();
+        dbContext.Events.Add(eventWithParticipants);
+        await dbContext.SaveChangesAsync();
+        
+        var handler = new EventParticipantQueryHandler(dbContext);
+
+        // Act
+        var result = await handler.Handle(query, CancellationToken.None);
+
+        // Assert
+        eventWithParticipants.EventParticipants!.First().Id.Should().Be(result.Data.First().Id);
+    }
+}
