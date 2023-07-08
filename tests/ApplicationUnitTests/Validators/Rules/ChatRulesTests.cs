b9d805bd343f7583d6745c2750cb1dcad456d89b
dimanoit <dimanoit@gmail.com>
2023-07-08 23:02:48 +0200

    chat rules tests

--- tests/ApplicationUnitTests/Validators/Rules/ChatRulesTests.cs
+++ tests/ApplicationUnitTests/Validators/Rules/ChatRulesTests.cs
@@ -11,20 +11,50 @@ namespace ApplicationUnitTests.Validators.Rules;
 
 public class ChatRulesTests
 {
+    private readonly IApplicationDbContext _dbContext;
+    
+    public ChatRulesTests()
+    {
+        _dbContext = Substitute.For<IApplicationDbContext>();
+    }
+
     [Fact]
     public async Task GetIsUserMemberOfChat_UserIsMember_ReturnsTrue()
     {
         // Arrange
-        var dbContext = Substitute.For<IApplicationDbContext>();
         var chat = ChatFaker.Create();
+        SetupChatsDbSet(new List<Chat> { chat });
 
-        var dbSet = new List<Chat> { chat }.AsQueryable().BuildMockDbSet();
-        dbContext.Chats.Returns(dbSet);
-        
         // Act
-        var isUserMemberOfChat = await dbContext.GetIsUserMemberOfChat(chat.Members[0], chat.Id, default);
+        var isUserMemberOfChat = await _dbContext.GetIsUserMemberOfChat(chat.Members[0], chat.Id, default);
 
         // Assert
         isUserMemberOfChat.Should().BeTrue();
+    }
+
+    [Fact]
+    public async Task GetIsUserAuthorOfMessage_UserIsAuthor_ReturnsTrue()
+    {
+        // Arrange
+        var message = MessageFaker.Create();
+        SetupMessagesDbSet(new List<Message> { message });
+
+        // Act 
+        var result = await _dbContext.GetIsUserAuthorOfMessage(message.SenderId, message.Id, default);
+
+        // Assert
+        result.Should().BeTrue();
+    }
+
+    private void SetupChatsDbSet(List<Chat> chats)
+    {
+        var dbSet = chats.AsQueryable().BuildMockDbSet();
+        _dbContext.Chats.Returns(dbSet);
+    }
+
+    private void SetupMessagesDbSet(List<Message> messages)
+    {
+        var dbSet = messages.AsQueryable().BuildMockDbSet();
+        _dbContext.Messages.Returns(dbSet);
     }
 }
