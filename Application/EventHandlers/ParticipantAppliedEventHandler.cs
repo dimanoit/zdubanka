77fed15be1e15edae52e5a15da20ff43b54c5786
dimanoit <dimanoit@gmail.com>
2023-08-30 09:15:29 +0200

    reformat code

--- Application/EventHandlers/ParticipantAppliedEventHandler.cs
+++ Application/EventHandlers/ParticipantAppliedEventHandler.cs
@@ -24,7 +24,7 @@ public class ParticipantAppliedEventHandler : INotificationHandler<ParticipantAp
             .Select(ap => ap.Organizer!.Email)
             .FirstAsync(cancellationToken);
 
-                                                var messageBody = $"User {notification.UserId} applied on your event";
+        var messageBody = $"User {notification.UserId} applied on your event";
 
         await _emailService.SendEmailAsync(null);
     }
